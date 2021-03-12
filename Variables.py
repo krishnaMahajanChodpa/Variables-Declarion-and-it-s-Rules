e4e78c6a158d656ea2d47186915de1917fd6d71d
Krishna_Mahajam_Chopda <80529533+krishnaMahajanChodpa@users.noreply.github.com>
2021-03-12 21:53:12 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,28 @@
+print("somiron mitra Sir ")
+
+# Q.1>- what is Variable
+# Ans - Variable simply bare nothing but value holders ,
+# Again variable will be forming as the building block in multiple things while programming in python.....
+
+# Rule of Variable --
+# Rule 1st -- No Space allow while declaring the variables..
+# Rule 2nd -- Variable Should not start with Numbes...
+#Rule 3rd -- Variable should not Contain Special Characters like - @,#,$,%,&,*....
+
+#Rule No 1 Example --
+# My Name='krishna'     <-- No Space allow While Declaring the variable..
+# print(My Name)        <-- We will get Error Message as invalid Syantax with Line Number...
+
+# Rule No 2 Example --
+# 1Name="krishna"       <-- Variable Should not start with Numbes...
+# print(1Name)          <-- We will get Error Message as invalid Syantax with Line Number...
+
+#Rule No 3 Example --
+# @Name='krishna'       <-- Variable should not Contain Special Characters like - @,#,$,%,&,*....
+# print(@Name)          <-- We will get Error Message as invalid Syantax with Line Number...
+
+
+
+
+
+
